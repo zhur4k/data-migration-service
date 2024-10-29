80169b48667fe469ad84c43074215bdcbd1e9c27
zhur4k <kostya.zhurovich12052@gmail.com>
2024-10-29 09:12:04 +0300

    feat: initialize Spring Boot project.

---
+++ src/main/java/com/datamigrationservice/DataMigrationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.datamigrationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataMigrationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataMigrationServiceApplication.class, args);
+    }
+
+}

1dc724b23c99e60c605c1de8c7ea0f268bd88e06
zhur4k <kostya.zhurovich12052@gmail.com>
2024-10-29 09:55:36 +0300

    chore: enable task scheduling with @EnableScheduling

--- src/main/java/com/datamigrationservice/DataMigrationServiceApplication.java
+++ src/main/java/com/datamigrationservice/DataMigrationServiceApplication.java
@@ -2,8 +2,10 @@ package com.datamigrationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class DataMigrationServiceApplication {
 
     public static void main(String[] args) {

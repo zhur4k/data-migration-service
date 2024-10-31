e4d20e952ecfd47e0bc84eda153acaf12687bfd0
zhur4k <kostya.zhurovich12052@gmail.com>
2024-10-31 14:13:35 +0300

    feat: add method to find by dob.

--- src/main/java/com/datamigrationservice/repository/PatientProfileRepository.java
+++ src/main/java/com/datamigrationservice/repository/PatientProfileRepository.java
@@ -4,6 +4,10 @@ import com.datamigrationservice.model.PatientProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.Optional;
+
 @Repository
 public interface PatientProfileRepository extends JpaRepository<PatientProfile, Long> {
+    Optional<PatientProfile> findByDob(LocalDate dob);
 }
